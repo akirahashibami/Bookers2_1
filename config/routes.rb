2d7b5eb706655462b428fd09db4cbc8e40895d40
akirahashibami <hashibami2207@gmail.com>
2020-07-14 06:24:10 +0000

    [Update] login User_show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+	root to: 'homes#index'
+
+	# homes#show に　home/about というURLを渡している
+  	get 'home/about' => 'homes#show'
+
+	devise_for :users
+
+
+	resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+	resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
